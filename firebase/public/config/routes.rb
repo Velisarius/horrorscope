fdb9704d7e2c6b85cca7a7b513fbb1e40bf6e320
Ameen Ajmeri <aajmeri@justworks.com>
2019-06-27 18:11:41 -0400

    fixed route

--- firebase/public/config/routes.rb
+++ firebase/public/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/send_notification' => 'application_controller#send_notification'
+  post '/send_notification' => 'application#send_notification'
 end
